70fdbda6eb027f12b1811d034e3dd91b45c4c6bd
Amanpreet Singh <amanpreetsingh2212@gmail.com>
2025-03-23 15:27:04 +0530

    refresh

--- analysis/analysis_src/missing_values.py
+++ analysis/analysis_src/missing_values.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -7,4 +6,19 @@ class missing_value(ABC):
     @abstractmethod
     def get_missing_value(self, df):
         pass
+
+class identify_missing_value(missing_value):
+    def get_missing_value(self, df):
+        print("\nMissing Values Count by Column:")
+        missing_values = df.isnull().sum()
+        print(missing_values)
+        pass
     
+if __name__ == "__main__":
+
+    # Load the data
+    df = pd.read_csv('/Users/amanpreetsingh/Desktop/VSCode/Market/extracted_data/NY-House-Dataset.csv')
+
+    # Perform Missing Values Analysis
+    obj1 = identify_missing_value()
+    obj1.get_missing_value(df)
